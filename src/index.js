ad513130a7091cbbfb0891b7557f3ee1de4b3362
Matheus Barone <matheusvbarone@gmail.com>
2018-09-10 20:40:09 -0300

    Creating smoke tests

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const search = () => { };
+const searchAlbuns = () => { };
+const searchArtists = () => { };
+const searchTracks = () => { };
+const searchPlaylists = () => { };
+
+export { search, searchAlbuns, searchArtists, searchTracks, searchPlaylists };
+
+
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,1 +1,28 @@
-// import { expect } from 'chai';
+import { expect } from 'chai';
+import { search, searchAlbuns, searchArtists, searchTracks, searchPlaylists } from '../src/index'
+
+describe('Spotify Wrapper', () => {
+  describe('Smoke Tests', () => {
+    // search (genérica) -> + de 1 tipo
+    // searchAlbuns
+    // searchArtists
+    // searchTracks
+    // searchPlaylists
+
+    it('should exists the search method', () => {
+      expect(search).to.exist;
+    })
+    it('should exists the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    })
+    it('should exists the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    })
+    it('should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    })
+    it('should exists the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    })
+  });
+});
